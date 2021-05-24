5af6c487da9164bda633b5793accb840edc6d84d
Eric Zimmerman <saericzimmerman@gmail.com>
2021-05-24 13:03:01 -0400

    nuget

--- RECmd/Properties/AssemblyInfo.cs
+++ RECmd/Properties/AssemblyInfo.cs
@@ -1,18 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-[assembly: AssemblyTitle("RECmd")]
-[assembly: AssemblyDescription("Command line Registry access")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Eric Zimmerman 501-313-3778")]
-[assembly: AssemblyProduct("RECmd")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("saericzimmerman@gmail.com")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -23,17 +11,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("b931c052-18f8-4be3-99f3-a974d78c46d2")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.6.0.0")]
-[assembly: AssemblyFileVersion("1.6.0.0")]
