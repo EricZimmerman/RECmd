a799b8bf4c30a726a7976d134f799193da322e89
cyberman1 <46872731+cyberman1@users.noreply.github.com>
2025-01-15 09:57:51 -0600

    Update BatchCsvOut.cs
    Proposed we modify the BatchCsvOut class to output the LastWriteTimestamp in the format yyyy-MM-dd HH:mm:ss.fffffff. 
    
    - Create a method GetFormattedTimestamp that formats the LastWriteTimestamp property.
    
    - Use the GetFormattedTimestamp method when writing the CSV file.

--- RECmd/BatchCsvOut.cs
+++ RECmd/BatchCsvOut.cs
@@ -1,27 +1,53 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 
-namespace RECmd;
-
-public class BatchCsvOut
+namespace RECmd
 {
-    public string HivePath { get; set; }
-    public string HiveType { get; set; }
-    public string Description { get; set; }
-    public string Category { get; set; }
-    public string KeyPath { get; set; }
-    public string ValueName { get; set; }
+    public class BatchCsvOut
+    {
+        public string HivePath { get; set; }
+        public string HiveType { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string KeyPath { get; set; }
+        public string ValueName { get; set; }
 
-    public string ValueType { get; set; }
-    public string ValueData { get; set; }
-    public string ValueData2 { get; set; }
-    public string ValueData3 { get; set; }
+        public string ValueType { get; set; }
+        public string ValueData { get; set; }
+        public string ValueData2 { get; set; }
+        public string ValueData3 { get; set; }
 
-    public string Comment { get; set; }
-    public bool Recursive { get; set; }
-    public bool Deleted { get; set; }
+        public string Comment { get; set; }
+        public bool Recursive { get; set; }
+        public bool Deleted { get; set; }
 
+        public DateTimeOffset? LastWriteTimestamp { get; set; }
 
-    public DateTimeOffset? LastWriteTimestamp { get; set; }
+        public string PluginDetailFile { get; set; }
 
-    public string PluginDetailFile { get; set; }
+        // Method to format the LastWriteTimestamp
+        public string GetFormattedTimestamp()
+        {
+            string dateTimeFormat = "yyyy-MM-dd HH:mm:ss.fffffff";
+            return LastWriteTimestamp?.ToString(dateTimeFormat, CultureInfo.InvariantCulture) ?? string.Empty;
+        }
+
+        // Method to write the CSV
+        public static void WriteCsv(string filePath, List<BatchCsvOut> data)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                // Write CSV header
+                writer.WriteLine("HivePath,HiveType,Description,Category,KeyPath,ValueName,ValueType,ValueData,ValueData2,ValueData3,Comment,Recursive,Deleted,LastWriteTimestamp,PluginDetailFile");
+
+                // Write CSV rows
+                foreach (var item in data)
+                {
+                    writer.WriteLine($"{item.HivePath},{item.HiveType},{item.Description},{item.Category},{item.KeyPath},{item.ValueName},{item.ValueType},{item.ValueData},{item.ValueData2},{item.ValueData3},{item.Comment},{item.Recursive},{item.Deleted},{item.GetFormattedTimestamp()},{item.PluginDetailFile}");
+                }
+            }
+        }
+    }
 }
